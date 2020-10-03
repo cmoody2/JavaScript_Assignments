7a6cf22c577796a802f8790ed8a5b536c1c0aceb
cmoody2 <cmoody.dev@gmail.com>
2020-10-02 18:24:29 -0700

    Update main.js

--- basic_javascript_projects/project1_expressions_alert/js/main.js
+++ basic_javascript_projects/project1_expressions_alert/js/main.js
@@ -1,2 +1,10 @@
-window.alert("Pepsi or Coke?");
+var darkSoda = 'Pepsi or Coke?'
+
+window.alert(darkSoda);
+
 document.write('Sprite or 7up?');
+
+var Soda = "This is Soda choices";
+
+document.write(Soda);
+
