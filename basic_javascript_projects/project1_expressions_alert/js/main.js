0e3bfdee94636143f84ae1fe6470bffab260faae
cmoody2 <cmoody.dev@gmail.com>
2020-10-03 14:53:49 -0700

    Update main.js
    added more assignment requirements

--- basic_javascript_projects/project1_expressions_alert/js/main.js
+++ basic_javascript_projects/project1_expressions_alert/js/main.js
@@ -1,10 +1,30 @@
-var darkSoda = 'Pepsi or Coke?'
+var darkSoda = 'Pepsi or Coke?'     //assigns the variable 'darkSoda' a string value
 
-window.alert(darkSoda);
+window.alert(darkSoda);         //creates a window alert displaying variable 'darkSoda
 
-document.write('Sprite or 7up?');
+document.write('Sprite or 7up?<br>');       //prints string inside page body
 
-var Soda = "This is Soda choices";
+var Soda = " This is Soda choices.<br><br>";        //assigns variable 'Soda' a string value
 
-document.write(Soda);
+document.write(Soda);                                  //prints variable 'Soda'
 
+document.write(" Lisa told Bart, \"Knock it off, Bart! Or I'll tell dad!\"<br>\"Eat my shorts!\" Bart responded.<br><br>")          //prints string that uses 'Escaped' characters using a "\"
+
+document.write("\"Be who you are and say what you feel,"+" because those who mind don\'t matter and those who matter don\'t mind.\""+" -Dr. Seuss<br><br>")     //prints a concatenation of several strings
+
+var A = "\"If your can print \"Hello World\", you can change the world\"" + " -Raghu Venkatesh<br><br>"                             //assigns variable 'A' two strings that are concatenated and use 'Escaped' characters
+document.write(A)
+
+var Family = "The Griffins", Dad = "Peter", Mom = "Lois", olderSon = "Chris", Daughter = "Meg", youngerSon = "Stewie";              //assigns mutliple variables in one statement each with their own values
+document.write(olderSon);
+
+var Family = Family.fontcolor("blue");                                                                                      //assigns a font color for each variable
+var Dad = Dad.fontcolor("green");
+var Mom = Mom.fontcolor("orange");
+var olderSon = olderSon.fontcolor("purple");
+var Daughter = Daughter.fontcolor("red");
+var youngerSon = youngerSon.fontcolor("yellow");
+document.write(Daughter)
+
+ var three = 3 + 3;                                                                                                     //assigns variable 'three' a value equal to the result of the expression
+ document.write(three);
