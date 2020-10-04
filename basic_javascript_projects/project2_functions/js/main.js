bbf36933a2b3014a5f75bf572617cff2b21d3b76
cmoody2 <cmoody.dev@gmail.com>
2020-10-03 18:36:38 -0700

    append_new_task

--- basic_javascript_projects/project2_functions/js/main.js
+++ basic_javascript_projects/project2_functions/js/main.js
@@ -5,3 +5,9 @@ function display() {                                                    //create
     var result = A +' and '+ B;                                             //assign variable 'result' equal to variables 'A' and 'B' concatenated
     document.getElementById('favKeyboards').innerHTML = result;              //assign document.getElementByID method an ID of 'favKeyboards'
 }
+
+function stringConcat() {                                               //create function 'stringConcat'
+    var sentence = "Keyboards are luxury items, ";                       //assign variable 'sentence' a string value
+    sentence += "but they save lives!";                                   //use "+=" operator to concatenate new string to variable 'sentence'
+    document.getElementById("concatenate").innerHTML = sentence;           //create method document.getElementById with id='concatenate'
+}
