66491e58a5591dbf1e18329a2ec59e453f528463
cmoody2 <cmoody.dev@gmail.com>
2020-10-03 20:09:34 -0700

    fix_method_spacing
    added space to end of the return for method

--- basic_javascript_projects/project3_math_operators/js/main.js
+++ basic_javascript_projects/project3_math_operators/js/main.js
@@ -1,4 +1,4 @@
 function add_func() {
     var addition = 10 + 10;
-    document.getElementById("math").innerHTML = "10 + 10 =" + addition;
+    document.getElementById("math").innerHTML = "10 + 10 = " + addition;
 }
