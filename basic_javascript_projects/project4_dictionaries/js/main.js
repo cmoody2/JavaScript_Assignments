c2f669df759a32f1529ada266dc393731a4786ae
cmoody2 <cmoody.dev@gmail.com>
2020-10-05 10:52:05 -0700

    update_projcet4
    add new dictionary tasks

--- basic_javascript_projects/project4_dictionaries/js/main.js
+++ basic_javascript_projects/project4_dictionaries/js/main.js
@@ -1,12 +1,13 @@
-function keeb_dictionary() {
-    var Keyboard = {
-        Name: "Exclusive E6.5",
-        Type: "65%",
-        Color: "Clear",
-        Material: "Polycarbonate",
-        Weight: "Brass",
-        Switches: "Zeal Roselios",
-        Price: 409.00,
+function keeb_dictionary() {                    //create function 'keeb_dictionary
+    var Keyboard = {                             //initiate an object named 'Keyboard'
+        Name: "Exclusive E6.5",                    //assign the key 'Name' a value of 'Exclusive E6.5
+        Type: "65%",                                //assign the key 'Type' a value of '65%'
+        Color: "Clear",                              //assign the key 'Color' a value of 'Clear'
+        Material: "Polycarbonate",                    //assign the key 'Material' a value of 'Polycarbonate'
+        Weight: "Brass",                               //assign the key 'Weight' a value of 'Brass'
+        Switches: "Zeal Roselios",                      //assign the key 'Switches' a value of 'Brass'
+        Price: 409.00,                                   //assign the key 'Price' a value of '490.00'
     };
-    document.getElementById("keeb").innerHTML = Keyboard.Material;
+    delete Keyboard.Type                                          //use 'delete' operator to del key 'Type' of object Keyboard
+    document.getElementById("keeb").innerHTML = Keyboard.Type;     //create method with id="keeb" to return value of key 'Type' in object 'Keyboard'
 }
