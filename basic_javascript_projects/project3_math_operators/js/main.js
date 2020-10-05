f90dfd07925ae85a373ce972a49b77ced8d3d541
cmoody2 <cmoody.dev@gmail.com>
2020-10-04 22:09:46 -0700

    update_project3_math_operators
    add new assignment tasks for JS project math operators

--- basic_javascript_projects/project3_math_operators/js/main.js
+++ basic_javascript_projects/project3_math_operators/js/main.js
@@ -23,3 +23,25 @@ function modulus() {                                            //create functio
     var remainder = 25 % 3;                                             //assign variable 'remainder' value equal to remainder of 25 / 3
     document.getElementById("mod").innerHTML = "When you divide 25 by 3 you get a remainder of : " + remainder;         //create method with id="mod" that returns string and variable 'remainder'
 }
+
+function neg_op() {                                             //create function 'neg_op'
+    var N = 25;                                                  //assign variable 'N' value of 25
+    document.getElementById("negative").innerHTML = -N;           //create method with id="negative" that returns negative number using the unary operator 'negation-operator'
+}
+
+function increment() {                                                      //create function 'increment'
+    var I = 35;                                                              //assign variable 'I' value of 35
+    I++;                                                                      //increment variable 'I' by one
+    document.getElementById("inc").innerHTML = I + " comes after 35!";         //create method with id="inc" that returns 35 incremented once
+}
+
+function decrement() {                                                              //create function 'decrement'
+    var V = 7.25;                                                                    //assign variable 'V' value of 7.25
+    V--;                                                                              //decrement variable 'V' by one
+    document.getElementById("dec").innerHTML = "7.25 - 1 is: " + V;                    //create method with id="dec" that returns 7.25 decreased by 1
+}
+
+function random_num() {                                                                                    //create function 'random_num'
+    var R = Math.random() * 50;                                                                            //assign variable 'R' value equal to random  number
+    document.getElementById("rand").innerHTML = "How many keyboards do I want to own? " + Math.floor(R);   //create method with id="rand" that returns random number passed to variable 'R' as the closet int rounded down 
+}
