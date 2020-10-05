751646acdda447cbd535347c38bbc295dc874e3c
cmoody2 <cmoody.dev@gmail.com>
2020-10-04 23:01:06 -0700

    create_project4_dictionaries
    create a new project for dictionary assignment tasks

---
+++ basic_javascript_projects/project4_dictionaries/js/main.js
@@ -0,0 +1,12 @@
+function keeb_dictionary() {
+    var Keyboard = {
+        Name: "Exclusive E6.5",
+        Type: "65%",
+        Color: "Clear",
+        Material: "Polycarbonate",
+        Weight: "Brass",
+        Switches: "Zeal Roselios",
+        Price: 409.00,
+    };
+    document.getElementById("keeb").innerHTML = Keyboard.Material;
+}
