72603c3d9e8c171b1b60d63682eaaa20ee935631
cmoody2 <cmoody.dev@gmail.com>
2020-10-06 23:23:57 -0700

    update_project6

--- basic_javascript_projects/project6_ternary_operators_constructors/js/main.js
+++ basic_javascript_projects/project6_ternary_operators_constructors/js/main.js
@@ -1,3 +1,5 @@
+//*TERNARY OPERATORS*
+
 function ride_function() {                                              //create function 'ride_function'
     var height, can_ride;                                                 //create variables 'height' and 'can_ride' 
     height = document.getElementById("height").value;                       //create method with id='height' to get user entered value and set equal to variable 'height'
@@ -13,17 +15,31 @@ function voteFunc() {                                                           
 }
 
 
-function Keyboard(make, designer, price, color) {
-    this.Keyboard_make = make;
-    this.Keyboard_designer = designer;
-    this.Keyboard_price = price;
-    this.Keyboard_color = color;
+//*OBJECT CONSTRUCTOR FUNCTION*
+
+function Keyboard(model, designer, price, color) {              //create class 'Keyboard' constructor with parameters: model, designer, price, color 
+    this.Keyboard_model = model;                                  //assign a potential object's 'model' property to the default class parameter 'model' using the "this" keyword to reference said object
+    this.Keyboard_designer = designer;                              //assign a potential object's 'designer' property to the default class parameter 'designer' using the "this" keyword to reference said object
+    this.Keyboard_price = price;                                      //assign a potential object's 'price' property to the default class parameter 'price' using the "this" keyword to reference said object
+    this.Keyboard_color = color;                                        //assign a potential object's 'color' property to the default class parameter 'color' using the "this" keyword to reference said object
 }
-var Chris = new Keyboard("Southpaw 65+", "Reconsiderit", 210, "Raw Aluminum");
-var Emily = new Keyboard("No.2", "Keycult", 900, "Dark Gray and Brass");
-var Erik = new Keyboard("Jane v2 CE", "TGR", 550, "Black");
-function keebsFunc() {
-    document.getElementById("new_and_this").innerHTML =
-    "Emily has a " + Emily.Keyboard_color + "-colored " + Emily.Keyboard_make +
+var Chris = new Keyboard("Southpaw 65+", "Reconsiderit", 210, "Raw Aluminum");    //create new object "Chris" with included properties using the constuctor function with the keyword "new"
+var Emily = new Keyboard("No.2", "Keycult", 900, "Dark Gray and Brass");            //create new object "Emily" with included properties using the constructor function with the keyword "new"
+var Erik = new Keyboard("Jane v2 CE", "TGR", 550, "Black");                           //create new object "Erik" with included properties using the constructor function with the keyword "new"
+function keebsFunc() {                                                                              //create function 'keebsFunc'
+    document.getElementById("new_and_this").innerHTML =                                               //create method with id="new_and_this" that returns concatenated string using the specified object's properties
+    "Emily has a " + Emily.Keyboard_color + "-colored " + Emily.Keyboard_model +
     " designed by " + Emily.Keyboard_designer + " that retailed for $" + Emily.Keyboard_price + ".";
+}
+
+
+//*NESTED FUNCTION*
+function compMemFunc() {                                   // create function 'compMemFunc'
+    document.getElementById("compMem").innerHTML = comp();   //create method that returns result of function 'comp'
+    function comp() {                                          //create function 'comp'
+        var samsung = 89;                                        //assign variable samsung a value of 89
+        function budget() {samsung += 10;}                         //create a nested function 'budget' which will use the variable 'samsung' in an expression
+        budget();                                                    //set results of nested function which would be samsung(99) = samsung(89) + 10
+        return samsung;                                                //return new value of variable 'samsung' from nested function 'budget'
+    }
 }
